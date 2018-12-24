1fa866bc18cb80d733e8486c453d99ecf8d6f408
cheeyang <cheeyang.teh@sc.com>
2018-12-24 14:48:51 +0800

    added firebase Authentication

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -4,6 +4,22 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { userLogin } from './actions';
 import { Link, withRouter } from 'react-router-dom';
+import { StyledFirebaseAuth } from 'react-firebaseui';
+
+
+const uiConfig = {
+    // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/browse',
+  // We will display Github as the auth provider.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+  ]
+}
 
 class Login extends Component {
     constructor(props) {
@@ -69,6 +85,7 @@ class Login extends Component {
         }
     }
 
+
     render() {
         return (
             <div className='formWrapper'>
@@ -89,6 +106,7 @@ class Login extends Component {
                         <Link to='./register'><button className='registerButton' type='submit'>Sign Up</button></Link>
                     </div>
                 </form>
+                <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
             </div>
         )
     }
