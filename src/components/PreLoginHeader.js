fb66ab189e24294ebeb8802aea2ad9b3b4991144
cheeyang <cheeyang.teh@sc.com>
2018-12-27 15:50:00 +0800

    edited rhcLogo to sample

--- src/components/PreLoginHeader.js
+++ src/components/PreLoginHeader.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const PreLoginHeader = () =>
     <div className='preLoginHeader'>
-        <img className='rhcLogo' src={require('resources/images/rhcLogo.png')}/>
+        <img className='rhcLogo' src={require('resources/images/rhcLogoSample.png')}/>
+        <div>window height: {window.innerHeight}</div>
+        <div>available height: {window.availHeight}</div>
+        <div>window width: {window.innerWidth}</div>
     </div>
 
 export default PreLoginHeader;
