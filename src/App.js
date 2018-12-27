833ebc736b1bf5590b1a45dd8e81a5dd92191f85
cheeyang <cheeyang.teh@sc.com>
2018-12-27 14:34:25 +0800

    added drawer, login/logout functioning correctly

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Register from './containers/Register';
 import Navbar from './components/Navbar';
 import Browse from './containers/Browse';
@@ -9,24 +9,39 @@ import AppFooter from './containers/AppFooter';
 import './App.css';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import firebase from 'firebase';
+import { updateUser } from 'containers/Login/actions';
+import { bindActionCreators } from 'redux';
 
-const App = (props) => {
-    const { user } = props.session;
-    return (
-        <Router>
-            <div className='AppWrapper'>
-                {!!user ? <Navbar/> : <PreLoginHeader/>}
-                <div className='AppBody'>
-                    <Route exact path='/' render={()=><Redirect to='/login'/>}/>
-                    <Route exact path='/login' component={Login}/>
-                    <Route exact path='/register' component={Register}/>
-                    <Route exact path='/browse' component={Browse}/>
-                    <Route exact path='/admin' component={AdminPage}/>
+class App extends Component {
+
+    constructor(props) {
+        super(props)
+
+        firebase.auth().onAuthStateChanged((user)=>{
+            console.log('auth state changed', user);
+            this.props.updateUser(user);
+        })
+    }
+    render() {
+        const { user } = this.props.session;
+        return (
+            <Router>
+                <div className='AppWrapper'>
+                    {!!user ? <Navbar/> : <PreLoginHeader/>}
+                    <div className='AppBody'>
+                        <Route exact path='/' render={()=><Redirect to='/login'/>}/>
+                        <Route exact path='/login' component={Login}/>
+                        <Route exact path='/register' component={Register}/>
+                        <Route exact path='/browse' component={Browse}/>
+                        <Route exact path='/admin' component={AdminPage}/>
+                    </div>
+                    <AppFooter/>
                 </div>
-                <AppFooter/>
-            </div>
-        </Router>
-    )
+            </Router>
+        )
+    }
+
 }
 
-export default connect(({session})=>({session}))(App);
+export default connect(({session})=>({session}), (dispatch)=>(bindActionCreators({updateUser}, dispatch)))(App);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Icon, Drawer } from 'antd'
+import firebase from 'firebase';
+import { withRouter } from 'react-router-dom';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import MDrawer from '@material-ui/core/Drawer';
 
-const Navbar = () =>
-    <div className='Navbar'>
-        NAVBAR COMPONENT
-    </div>
+const sideList = (
+      <div>
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </div>
+    );
 
-export default Navbar;
+class Navbar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            drawerVisible: false
+        }
+    }
+
+    _onPressLogout = () => {
+        firebase.auth().signOut();
+        this.props.history.push('/');
+    }
+
+    _openDrawer = () => {
+        console.log('open drawer');
+        this.setState({drawerVisible:true});
+    }
+
+    _closeDrawer = () => {
+        this.setState({drawerVisible:false});
+    }
+
+
+    render(){
+        return (
+            <div className='NavbarContainer'>
+                <MDrawer open={this.state.drawerVisible} onClose={this._closeDrawer}>
+                <div onClick={this._closeDrawer} onKeyDown={this._closeDrawer}>
+                    {sideList}
+                </div>
+                </MDrawer>
+                <button className='drawerIcon' onClick={this._openDrawer}>
+                    <Icon className='icon' type="menu-unfold"/>
+                </button>
+                <div className='Navbar'>
+                    <button className='Navbar-item' onClick={()=>this.props.history.push('/browse')}>Browse</button>
+                </div>
+                <button className='LogOutIcon' onClick={this._onPressLogout}>
+                    <Icon className='icon' type="poweroff"/>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default withRouter(Navbar);
