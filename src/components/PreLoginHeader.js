f8349a3384eff961b8e771faa428eea62ed036c1
cheeyang <cheeyang.teh@sc.com>
2018-12-27 17:43:17 +0800

    removed height and width displaying

--- src/components/PreLoginHeader.js
+++ src/components/PreLoginHeader.js
@@ -3,9 +3,6 @@ import React from 'react';
 const PreLoginHeader = () =>
     <div className='preLoginHeader'>
         <img className='rhcLogo' src={require('resources/images/rhcLogoSample.png')}/>
-        <div>window height: {window.innerHeight}</div>
-        <div>available height: {window.availHeight}</div>
-        <div>window width: {window.innerWidth}</div>
     </div>
 
 export default PreLoginHeader;
