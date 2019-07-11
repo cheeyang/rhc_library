1d4a58c552119ab92e37d9c3ed9ba09b48df5d16
chee yang <tehcheeyang@gmail.com>
2019-07-11 16:47:57 +0800

    darken borrowed books

--- src/components/BookInfoCard.js
+++ src/components/BookInfoCard.js
@@ -1,37 +1,53 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import { selectBook } from 'containers/Browse/actions';
-import { withRouter } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { selectBook } from "containers/Browse/actions";
+import { withRouter } from "react-router-dom";
 
 class BookInfoCard extends Component {
-    _selectedStyle = (isHighlighted) => {
-        if (isHighlighted){
-            return {backgroundColor:'#FFFFFF12'};
-        } else {
-            return null;
-        }
+  _styleSelector = isSelected => {
+    if (this.props.book.loanData !== null) {
+      //if book has been borrowed, i.e. loanData is present
+      return { opacity: 0.4, pointerEvents: "none" };
+    } else {
+      //if book is still available
+      return isSelected ? { backgroundColor: "#FFFFFF12" } : null;
     }
-    render() {
-        const { book, key } = this.props;
-        const isHighlighted = this.props.browse.bookIdsSelected.includes(book.id) && this.props.location.pathname!=='/confirmation'
-        return (
-            <div style={this._selectedStyle(isHighlighted)} className='listItem' onClick={()=>this.props.selectBook(book.id)} key={key}>
-                <p>Title:</p>
-                <p>{book.title}</p>
-                <p>Author:</p>
-                <p>{book.author}</p>
-                <p>Tags:</p>
-                <p>{book.tags.map(obj=>obj.label).join(', ')}</p>
-                <p>ISBN:</p>
-                <p>{book.isbn}</p>
-                {isHighlighted
-                    ? <img className='borrowedIcon' src={require('resources/images/tickIcon.png')}/>
-                    : null
-                }
-            </div>
-        )
-    }
+  };
+  render() {
+    const { book, key } = this.props;
+    const isSelected =
+      this.props.browse.bookIdsSelected.includes(book.id) &&
+      this.props.location.pathname !== "/confirmation";
+    return (
+      <div
+        style={this._styleSelector(isSelected)}
+        className="listItem"
+        onClick={() => this.props.selectBook(book.id)}
+        key={key}
+      >
+        <p>Title:</p>
+        <p>{book.title}</p>
+        <p>Author:</p>
+        <p>{book.author}</p>
+        <p>Tags:</p>
+        <p>{book.tags.map(obj => obj.label).join(", ")}</p>
+        <p>ISBN:</p>
+        <p>{book.isbn}</p>
+        {isSelected ? (
+          <img
+            className="borrowedIcon"
+            src={require("resources/images/tickIcon.png")}
+          />
+        ) : null}
+      </div>
+    );
+  }
 }
 
-export default withRouter(connect(({browse})=>({browse}),(dispatch)=>bindActionCreators({selectBook},dispatch))(BookInfoCard));
+export default withRouter(
+  connect(
+    ({ browse }) => ({ browse }),
+    dispatch => bindActionCreators({ selectBook }, dispatch)
+  )(BookInfoCard)
+);
